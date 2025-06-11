4e1c6d243caba17bdecd326b1c3e3f88c2a415ca
Angelo Mendes <angelo030825@gmail.com>
2025-06-11 07:46:51 -0300

    Primeiro commit

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.secondary
+                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+            Text("Your content")
         }
-        .padding()
+        .background(.blue)
     }
 }
 
